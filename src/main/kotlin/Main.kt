63e6d8f77caea9067f4e4f79d34e5b29090cb323
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-08-30 22:49:33 +0200

    Use real puzzle input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import okio.FileSystem
 import okio.Path.Companion.toPath
 
 fun main(args: Array<String>) {
-    val input = FileSystem.SYSTEM.read("test_input.txt".toPath()) { readUtf8() }.dropLast(1)
+    val input = FileSystem.SYSTEM.read("input.txt".toPath()) { readUtf8() }.dropLast(1)
     val matrix = input.split("\n").map { line -> line.asIterable().map { it.toString().toInt() } }
     val matrixSize = matrix.size
 
