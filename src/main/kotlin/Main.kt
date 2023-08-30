4f3c1d8db5aa7123c7c8d304d3b698f4720c3bad
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-08-30 23:07:38 +0200

    Improve comment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,7 +15,9 @@ fun main(args: Array<String>) {
             val row = matrix[rowNumber]
             val column = matrix.map { it[columnNumber] }
 
-            // Map the tree to its visibility from all 4 directions (left, right, top, bottom)
+            // Map the tree to its visibility. A tree is visible if it is visible at least from 1 direction (left,
+            // right, top, bottom). For a tree to be visible from a direction, all trees between it and the edge must be
+            // lower than this tree.
             row.take(columnNumber).all { it < tree } ||
                     row.takeLast(matrixSize - 1 - columnNumber).all { it < tree } ||
                     column.take(rowNumber).all { it < tree } ||
