d8e92d9673fb62489555c9b11bfde209cee56018
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-08-30 23:20:09 +0200

    Extract row to outer loop; use asSequence() before take()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import okio.FileSystem
 import okio.Path.Companion.toPath
+import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     val input = FileSystem.SYSTEM.read("input.txt".toPath()) { readUtf8() }.dropLast(1)
@@ -9,21 +10,25 @@ fun main(args: Array<String>) {
     // all trees on the edges are visible
     val visibleOnEdges = 4 * (matrixSize - 1)
 
-    val visibleInside = (1..<matrixSize - 1).flatMap { rowNumber ->
-        (1..<matrixSize - 1).map { columnNumber ->
-            val tree = matrix[rowNumber][columnNumber]
+    val time = measureTimeMillis {
+        val visibleInside = (1..<matrixSize - 1).flatMap { rowNumber ->
             val row = matrix[rowNumber]
-            val column = matrix.map { it[columnNumber] }
 
-            // Map the tree to its visibility. A tree is visible if it is visible at least from 1 direction (left,
-            // right, top, bottom). For a tree to be visible from a direction, all trees between it and the edge must be
-            // lower than this tree.
-            row.take(columnNumber).all { it < tree } ||
-                    row.takeLast(matrixSize - 1 - columnNumber).all { it < tree } ||
-                    column.take(rowNumber).all { it < tree } ||
-                    column.takeLast(matrixSize - 1 - rowNumber).all { it < tree }
-        }
-    }.count { it }
+            (1..<matrixSize - 1).map { columnNumber ->
+                val tree = matrix[rowNumber][columnNumber]
+                val column = matrix.map { it[columnNumber] }
 
-    println(visibleOnEdges + visibleInside)
+                // Map the tree to its visibility. A tree is visible if it is visible at least from 1 direction (left,
+                // right, top, bottom). For a tree to be visible from a direction, all trees between it and the edge must be
+                // lower than this tree.
+                row.asSequence().take(columnNumber).all { it < tree } ||
+                        row.takeLast(matrixSize - 1 - columnNumber).all { it < tree } ||
+                        column.asSequence().take(rowNumber).all { it < tree } ||
+                        column.takeLast(matrixSize - 1 - rowNumber).all { it < tree }
+            }
+        }.count { it }
+
+        println(visibleOnEdges + visibleInside)
+    }
+    println("Time: $time ms")
 }
