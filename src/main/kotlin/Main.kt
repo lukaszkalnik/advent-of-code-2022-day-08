315fc689f27563bd5bd4312ffffe7ae3b6032101
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-08-30 22:45:03 +0200

    Working solution for test_input.txt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,22 @@ import okio.Path.Companion.toPath
 fun main(args: Array<String>) {
     val input = FileSystem.SYSTEM.read("test_input.txt".toPath()) { readUtf8() }.dropLast(1)
     val matrix = input.split("\n").map { line -> line.asIterable().map { it.toString().toInt() } }
-    matrix.forEach { row ->
-        println(row)
+    val matrixSize = matrix.size
+
+    var totalVisible = 4 * (matrixSize - 1)
+
+    (1..<matrixSize - 1).map { rowNumber ->
+        (1..<matrixSize - 1).map { columnNumber ->
+            val tree = matrix[rowNumber][columnNumber]
+            val row = matrix[rowNumber]
+            val column = matrix.map { it[columnNumber] }
+            val visible = row.take(columnNumber).all { it < tree } ||
+                    row.takeLast(matrixSize - 1 - columnNumber).all { it < tree } ||
+                    column.take(rowNumber).all { it < tree } ||
+                    column.takeLast(matrixSize - 1 - rowNumber).all { it < tree }
+            if (visible) ++totalVisible
+        }
     }
+
+    println(totalVisible)
 }
